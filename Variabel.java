ff23cdd4ac1b8a7d78dd7ddcceebe81b894a6922
alvoera2 <115127713+alvoera2@users.noreply.github.com>
2022-11-24 11:00:14 +0700

    create Variabel.java
    tugas uts

---
+++ Variabel.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        String salam = "Assalamualaikum";
+        String nama = "Alim Purnomo Aji Surya M.R";
+        int lahir = 2003;
+        
+        System.out.println(salam);
+        System.out.println("Nama Saya "+nama);
+        System.out.println("Saya Lahir di Tahun : "+lahir);
+    }
+}
